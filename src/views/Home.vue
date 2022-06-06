69cc8da5c5f53cea74f16e44eed50a6406c68b86
a3a4aa5 <linlgd@outlook.com>
2022-06-06 15:21:34 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Home">init</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.background {
+  min-height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url('@/assets/img/day/day-clear.jpg');
+}
+</style>
